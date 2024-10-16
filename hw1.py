1fac3cd2fb138706509bcd35e1106fd042ea3d55
Tacha <Anastasiykoshe@gmail.com>
2024-10-16 13:45:00 +0300

    Модуль 12 задание 1

---
+++ hw1.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest import TestCase
+from forTest import Runner
+
+
+class RunnerTest(TestCase):
+    def test_walk(self):
+        runner = Runner('John')
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner('Hamish')
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner('William')
+        runner2 = Runner('Sherlock')
+        for _ in range(10):
+            runner1.walk()
+        for _ in range(10):
+            runner2.run()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
