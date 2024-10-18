94709460d99bfa018f34b00bebff3322009dc159
Tacha <Anastasiykoshe@gmail.com>
2024-10-18 16:50:46 +0300

    Модуль 12 задание 2

---
+++ hw2.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+from forTest2 import Runner, Tournament
+
+
+class TournamentTest(TestCase):
+    all_results = {}
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runner1 = Runner('Усейн', 10)
+        self.runner2 = Runner('Андрей', 9)
+        self.runner3 = Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for name, result in cls.all_results.items():
+            print(f'{name}: {result}')
+
+    def test_1(self):
+        tournament = Tournament(90, self.runner1, self.runner3)
+        finishers = tournament.start()
+        last_finisher = finishers[max(finishers.keys())]
+        last_finisher_name = last_finisher.name
+        self.assertTrue(last_finisher_name == 'Ник')
+
+    def test_2(self):
+        tournament = Tournament(90, self.runner2, self.runner3)
+        finishers = tournament.start()
+        last_finisher = finishers[max(finishers.keys())]
+        last_finisher_name = last_finisher.name
+        self.assertTrue(last_finisher_name == 'Ник')
+
+    def test_3(self):
+        tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
+        finishers = tournament.start()
+        last_finisher = finishers[max(finishers.keys())]
+        last_finisher_name = last_finisher.name
+        self.assertTrue(last_finisher_name == 'Ник')
